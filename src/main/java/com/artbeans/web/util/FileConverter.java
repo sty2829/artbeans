fb5926b080532d65e7a2c129c86e9623a6b97044
sty2829 <sim2829@gmail.com>
2021-04-06 20:03:03 +0900

    파일공통 패스 수정

--- src/main/java/com/artbeans/web/util/FileConverter.java
+++ src/main/java/com/artbeans/web/util/FileConverter.java
@@ -6,10 +6,17 @@ import com.artbeans.web.entity.FileInfo;
 
 import lombok.extern.slf4j.Slf4j;
 
+/*
+ * 파일공통입니다. 
+ */
 @Slf4j
 public class FileConverter {
 	
-	private final static String ROOT = "C:\\simstudy\\workspace\\artbeans\\src\\main\\webapp\\resources\\assets\\img\\";
+	/*
+	 * 파일위치 안건드리셔도 됩니다.
+	 */
+	private final static String PROJECT_PATH = System.getProperty("user.dir");
+	private final static String ROOT = "\\src\\main\\webapp\\resources\\assets\\img\\";
 	
 	public static void fileInsert(FileInfo fileInfo, String fiType) throws Exception {
 		String fiName = fileInfo.getFiFile().getOriginalFilename();
@@ -19,13 +26,15 @@ public class FileConverter {
 		}
 		int idx = fiName.lastIndexOf(".");
 		String extName = fiName.substring(idx);
-		String path = (ROOT+fiType+"\\\\") + System.nanoTime() + extName;
+		String path = System.nanoTime() + extName;
 		fileInfo.setFiOriginalname(fiName);
 		fileInfo.setFiSize(fiSize);
 		fileInfo.setFiPath(path);
 		fileInfo.setFiType(fiType);
 		log.info("fileInfo=>{}", fileInfo);
-		File file = new File(path);
+		log.info("path => {}", PROJECT_PATH + ROOT + fiType + "\\\\" + path );
+		File file = new File(PROJECT_PATH + ROOT + fiType + "\\\\" + path);
 		fileInfo.getFiFile().transferTo(file);
+		
 	}
 }
